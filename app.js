35fe2ff3801a278fb20b383988d1a075d9662142
Alex Normand <anormnd@gmail.com>
2012-06-17 17:43:56 +0200

    main template design

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express'),
+    stylus  = require('stylus'),
+    nib     = require('nib'),    
+    routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
@@ -15,7 +17,16 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(stylus.middleware({
+      src: __dirname + '/public',
+      compile: function(str, path) {
+          return stylus(str)
+              .set('filename', str)
+              .set('compress', true)
+              .use(nib())
+              .import('nib');          
+      }
+  }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -35,3 +46,21 @@ app.get('/', routes.index);
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+
+
+/*
+ function ValidURL(str) {
+  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+  if(!pattern.test(str)) {
+    alert("Please enter a valid URL.");
+    return false;
+  } else {
+    return true;
+  }
+}
+ */
