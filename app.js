20cb6c4d65f18693e7f246268396ac6f1c0f1446
Alex Normand <anormnd@gmail.com>
2012-06-18 00:22:37 +0200

    code cleanup

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
+app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(stylus.middleware({
       src: __dirname + '/public',
-      compile: function(str, path) {
+      compile: function (str, path) {
           return stylus(str)
               .set('filename', str)
               .set('compress', true)
@@ -31,11 +31,11 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
+app.configure('production', function () {
   app.use(express.errorHandler());
 });
 
@@ -45,24 +45,6 @@ app.get('/:text', routes.getQRCodeImage);
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
-
-
-/*
- function ValidURL(str) {
-  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-    '(\#[-a-z\d_]*)?$','i'); // fragment locater
-  if(!pattern.test(str)) {
-    alert("Please enter a valid URL.");
-    return false;
-  } else {
-    return true;
-  }
-}
- */
